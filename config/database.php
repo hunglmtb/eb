ce9121bab6fcb8a84a2044b94c8100bdd1ba623e
admin <admin>
2016-02-04 18:40:34 +0000

    add oracle config

--- config/database.php
+++ config/database.php
@@ -27,8 +27,8 @@ return [
 	*/
 
 	// 'default' => 'mysql',
-	'default' => 'sqlsrv',
-	// 'default' => 'oracle',
+	// 'default' => 'sqlsrv',
+	'default' => 'oracle',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -84,17 +84,7 @@ return [
 			'password' => env('DB_PASSWORD', '123456'),
 			'prefix'   => '',
 		],
-		'oracle' => [
-			'driver'   => 'oracle',
-			'tns'      => env('DB_TNS', ''),
-			'host'     => env('DB_HOST', ''),
-			'port'     => env('DB_PORT', '1521'),
-			'database' => env('DB_DATABASE', ''),
-			'username' => env('DB_USERNAME', ''),
-			'password' => env('DB_PASSWORD', ''),
-			'charset'  => env('DB_CHARSET', 'AL32UTF8'),
-			'prefix'   => env('DB_PREFIX', ''),
-		],
+		
 
 	],
 
--- config/oracle.php
+++ config/oracle.php
@@ -2,14 +2,14 @@
 
 return [
     'oracle' => [
-        'driver'   => 'oracle',
-        'tns'      => env('DB_TNS', ''),
-        'host'     => env('DB_HOST', ''),
-        'port'     => env('DB_PORT', '1521'),
-        'database' => env('DB_DATABASE', ''),
-        'username' => env('DB_USERNAME', ''),
-        'password' => env('DB_PASSWORD', ''),
-        'charset'  => env('DB_CHARSET', 'AL32UTF8'),
-        'prefix'   => env('DB_PREFIX', ''),
-    ],
+			'driver'   => 'oracle',
+			'tns'      => env('DB_TNS', ''),
+			'host'     => env('DB_HOST', ''),
+			'port'     => env('DB_PORT', '1521'),
+			'database' => env('DB_DATABASE', 'ENERGYBUILDE'),
+			'username' => env('DB_USERNAME', 'SYSTEM'),
+			'password' => env('DB_PASSWORD', '123456'),
+			'charset'  => env('DB_CHARSET', 'AL32UTF8'),
+			'prefix'   => env('DB_PREFIX', ''),
+	],
 ];
