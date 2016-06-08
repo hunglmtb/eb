061216e66b7c9fddad13d817cc7ca61c1954abd6
Hung Le <hunglmtb@gmail.com>
2016-06-08 11:01:52 +0700

    fix save date_end

--- app/Http/Middleware/SaveWorkspace.php
+++ app/Http/Middleware/SaveWorkspace.php
@@ -13,10 +13,10 @@ class SaveWorkspace {
 	 */
 	public function handle($request, Closure $next)
 	{
-		$wp = $request->only('date_begin', 'Facility');
+		$wp = $request->only('date_begin', 'Facility','date_end');
 		$currentUser = auth()->user();
 		if ($currentUser) {
-			$currentUser->saveWorkspace($wp['date_begin'],$wp['Facility']);
+			$currentUser->saveWorkspace($wp['date_begin'],$wp['Facility'],$wp['date_end']);
 		}
 		return $next($request);
 	}
