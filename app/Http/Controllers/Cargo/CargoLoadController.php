6c53aeeca3e36cd0cd7bd9adac7eb14b1a4e29d1
Hung Le <hunglmtb@gmail.com>
2016-12-22 14:42:09 +0700

    fix empty date_end 2 now

--- app/Http/Controllers/Cargo/CargoLoadController.php
+++ app/Http/Controllers/Cargo/CargoLoadController.php
@@ -21,7 +21,7 @@ class CargoLoadController extends CodeController {
     public function getDataSet($postData,$dcTable,$facility_id,$occur_date,$properties){
     	$storage_id		= $postData['Storage'];
     	$date_end 		= $postData['date_end'];
-    	$date_end 		= \Helper::parseDate($date_end);
+    	$date_end		= $date_end&&$date_end!=""?\Helper::parseDate($date_end):Carbon::now();
     	
     	$mdlName 		= $postData[config("constants.tabTable")];
     	$mdl 			= "App\Models\\$mdlName";
