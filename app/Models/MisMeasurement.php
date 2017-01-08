7c4426faaa9581ded1f436130f0b365cf26f364f
Tung Nguyen <tung0980@gmail.com>
2017-01-09 00:11:34 +0700

    tung modify mmr

--- app/Models/MisMeasurement.php
+++ app/Models/MisMeasurement.php
@@ -68,4 +68,36 @@ use App\Models\EbBussinessModel;
 		}
 		if ($shouldSave) $this->save();
 	}
+	public static function getForeignColumn($row,$originCommand,$columnName){
+		$command = $originCommand;
+		if ($columnName=="OBJECT_ID") {
+			/* $s_where	= array_key_exists('where', $oColumns)?$oColumns['where']:"";
+			$s_order	= array_key_exists('order', $oColumns)?$oColumns['order']:""; */
+			$s_where	= "";
+			$s_order	= "";
+			$namefield	= "NAME";
+			$id			= $row&&array_key_exists('OBJECT_TYPE', $row)?$row["OBJECT_TYPE"]:1;
+			$objType	= IntObjectType::find($id);
+			if($objType&&$objType->CODE) {
+				$ref_table	= $objType->CODE;
+				$command 	= "select ID, $namefield from `$ref_table` $s_where $s_order ; --select";
+			}
+		}
+		return $command;
+	}
+	
+	public static function getDependences($columnName,$idValue){
+		$option = null;
+		if ($columnName=="OBJECT_TYPE") {
+			$option = ["dependences"	=> [["name"		=>"ObjectName",
+											"elementId"	=> "OBJECT_ID",
+											]],
+						"sourceModel"	=> "IntObjectType",
+						"targets"		=> ["OBJECT_ID"],
+						'extra'			=> ["OBJECT_TYPE"],
+					];
+					
+		}
+		return $option;
+	}
 } 
