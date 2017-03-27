26e358aeae78e995a57e842f35167d9050dde9e9
Hung Le <hunglmtb@gmail.com>
2017-03-27 17:00:59 +0700

    fix cfg

--- app/Models/CfgFieldProps.php
+++ app/Models/CfgFieldProps.php
@@ -16,18 +16,20 @@ class CfgFieldProps extends DynamicModel
     	$should	= !$object||$this->RANGE_PERCENT&&$this->RANGE_PERCENT>0;
    	 	if (!$should) {
    	 		$objectExtension = isset ( $this->OBJECT_EXTENSION)?json_decode ($this->OBJECT_EXTENSION,true):[];
-   	 		$found	= current(array_filter(array_keys($objectExtension), function($key) use($object,$objectExtension) { 
-			    			$result	= $object->{$object::$idField}==$key;
-			    			if ($result) {
-			    				$overwrite 	= array_key_exists("OVERWRITE", $objectExtension[$key])?$objectExtension[$key]["OVERWRITE"]:false;
-			    				$basic 		= array_key_exists("basic", $objectExtension[$key])?$objectExtension[$key]["basic"]:[];
-			    				$result		= ($overwrite==true||$overwrite=="true")
-			    								&&array_key_exists("RANGE_PERCENT", $basic)
-			    								&&$basic["RANGE_PERCENT"]>0;
-			    			}
-			    			return $result;
-			    	}));
-   	 		$should =	$found!==FALSE;
+   	 		if($objectExtension&&is_array($objectExtension)){
+	   	 		$found	= current(array_filter(array_keys($objectExtension), function($key) use($object,$objectExtension) { 
+				    			$result	= $object->{$object::$idField}==$key;
+				    			if ($result) {
+				    				$overwrite 	= array_key_exists("OVERWRITE", $objectExtension[$key])?$objectExtension[$key]["OVERWRITE"]:false;
+				    				$basic 		= array_key_exists("basic", $objectExtension[$key])?$objectExtension[$key]["basic"]:[];
+				    				$result		= ($overwrite==true||$overwrite=="true")
+				    								&&array_key_exists("RANGE_PERCENT", $basic)
+				    								&&$basic["RANGE_PERCENT"]>0;
+				    			}
+				    			return $result;
+				    	}));
+	   	 		$should =	$found!==FALSE;
+   	 		}
     	}
     	return $should;
     }
