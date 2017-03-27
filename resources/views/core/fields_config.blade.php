59a292d72ec61357a18f84f0b02a9842977bebb3
Hung Le <hunglmtb@gmail.com>
2017-03-27 17:30:06 +0700

    fix cfg

--- resources/views/core/fields_config.blade.php
+++ resources/views/core/fields_config.blade.php
@@ -191,10 +191,15 @@ var _fieldconfig = {
 				var objectExtension	= data[0].OBJECT_EXTENSION;
 				_fieldconfig.objectExtensionTarget = respondData.objectExtensionTarget;
 				if(objectExtension!=null&&objectExtension!=""){
-					var objects = $.parseJSON(objectExtension);
-					$.each(objects, function( index, value ) {
-						_fieldconfig.addObjectExtension(objectExtensionSource,value,index);
-					});
+					try {
+						var objects = $.parseJSON(objectExtension);
+						$.each(objects, function( index, value ) {
+							_fieldconfig.addObjectExtension(objectExtensionSource,value,index);
+						});
+					}
+					catch(err) {
+					    console.log("can not parse  objectExtension +\n"+err.message);
+					}
 				}
 				
 				var addObjectBtn = $("<img id='addObjectBtn'></img>");
