e95e6121ff4c59a89236e1cc48881669cdd1fd77
Tung Nguyen <tung0980@gmail.com>
2017-04-10 19:03:54 +0700

    Tung delete unused public files

--- public/wf/includes/gfinnit.php
+++
@@ -1,9 +0,0 @@
-<?php
-	define("HOSTNAME","localhost");
-	define("DB_USERNAME","nhneu_tung");
-	define("DB_PASSWORD","tung#3");
-	define("DB_DATANAME","eb");
-	// innit com_language
-	define("IS_ACTIVE_LANG",true);
-
-?>
